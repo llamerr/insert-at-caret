18414357712d43719baad31f8132f2c44c691a75
Alexey Lukin <alexey.lukin@da-14.com>
2018-11-01 22:29:40 +0200

    Update index.js
    - use execCommand so edits are undoable
    - re-focus so textarea scrolled to cursor position

--- index.js
+++ index.js
@@ -3,23 +3,9 @@ function insertAtCaret(txtarea, text, addspace) {
         return;
     }
 
-    var front = txtarea.value.substring(0, txtarea.selectionStart);
-    var back = txtarea.value.substring(txtarea.selectionEnd, txtarea.value.length);
-
-    if (addspace && front.length && !front.match(/\s$/)) {
-        text = ' ' + text;
-    }
-
-    txtarea.value = front + text + back;
-
-    var strPos = (front + text).length;
-
-    txtarea.selectionStart = strPos;
-    txtarea.selectionEnd = strPos;
+    document.execCommand('insertText', false, text);
+    txtarea.blur();
     txtarea.focus();
-
-    var event = new Event('keyup');
-    txtarea.dispatchEvent(event);
 }
 
 export default insertAtCaret;
